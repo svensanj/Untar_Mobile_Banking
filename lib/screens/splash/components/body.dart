f94c50659057f735f6aa6ce636416f2a794f1149
Lisa <71247497+erymac@users.noreply.github.com>
2021-06-02 08:47:28 +0700

    Update body.dart

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,72 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:untar_mobile_banking/screens/sign_in/sign_in_screen.dart';
+import 'package:untar_mobile_banking/components/no_account_text.dart';
 import '../../../size_config.dart';
-import '../../../components/default_button.dart';
+import 'sign_form.dart';
 
-class Body extends StatefulWidget {
-  @override
-  _BodyState createState() => _BodyState();
-}
-
-class _BodyState extends State<Body> {
+class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Padding(
-      padding: EdgeInsets.all(16.0),
       child: SizedBox(
         width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(top: 80, bottom: 100),
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Bank",
-                      style: TextStyle(
-                        fontSize: 50.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue[600],
-                      ),
-                    ),
-                    Text(
-                      "💲Ratnu💲",
-                      style: TextStyle(
-                        fontSize: 70.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue[800],
-                      ),
-                    ),
-                  ],
+        child: Padding(
+          padding:
+              EdgeInsets.symmetric(horizontal: getProportionateScreenWidth(20)),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.05),
+                Text(
+                  "Welcome Back",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Padding(
-                padding: EdgeInsets.symmetric(
-                    horizontal: getProportionateScreenWidth(20)),
-                child: Column(
-                  children: <Widget>[
-                    Spacer(),
-                    Spacer(flex: 3),
-                    DefaultButton(
-                      text: "Continue",
-                      press: () {
-                        Navigator.pushNamed(context, SignInScreen.routeName);
-                      },
-                    ),
-                    Spacer(),
-                  ],
+                Text(
+                  "Sign in with your email and password  \nor continue with social media",
+                  textAlign: TextAlign.center,
                 ),
-              ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SizedBox(height: getProportionateScreenHeight(20)),
+                NoAccountText(),
+              ],
             ),
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
